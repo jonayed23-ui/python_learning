e79db7119862e019f8a1d2f0576da819647c907f
jonayed23-ui <jonayeda592@gmail.com>
2026-02-05 21:53:47 +0600

    variabe in python

---
+++ pyhon_variable.py
@@ -0,0 +1,12 @@
+name = "jonayed"
+age = 25
+print("My name is:", name)
+print("My age is:",age)
+"'Change Variable Value Python allows changing value anytime'"
+x= 5
+x = "jonayed"
+print(x)
+#Delete a Variable
+x = 10
+del x
+print(x) #This will raise an error beacuse x is deleted
