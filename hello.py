d6a9648b9304ed93f1568fdafdd67c0bf1a54c25
jonayed23-ui <jonayeda592@gmail.com>
2026-02-05 20:25:11 +0600

    single and multiline comment learn

---
+++ hello.py
@@ -0,0 +1,11 @@
+# This is a multiline comment
+# written using hash symbol
+# each line starts with #
+"""
+This looks like a multiline comment
+but actually it is a multi-line string
+Python ignores it if not assigned
+"""
+# This program prints Hello World
+
+print("Hello, World!")
