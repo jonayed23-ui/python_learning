8e864668b937fd82231549c0a0f3e479e02626d7
jonayed23-ui <jonayeda592@gmail.com>
2026-06-21 22:33:40 +0600

    Initial commit

---
+++ operator.py
@@ -0,0 +1,26 @@
+#Arithmetic Operator
+a = 290
+b = 10
+print( a + b) #add
+print( a - b) #subtract
+print( a * b) #multiply
+print( a / b) #divide
+print( a % b) #modulo
+
+x = 5
+y = 2
+print( x ** y) #exponentiation 5*5
+
+
+
+i = 15
+k = 3
+print( i // k) #floor division Floor = সবচেয়ে বড় পূর্ণসংখ্যা যা এই মানের চেয়ে ছোট বা সমান
+
+j = -10
+print( j // k)
+
+#Assignment Operator
+
+
+
